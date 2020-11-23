156a542cc118c7de6e973074e07929728893a883
alsals126 <9970mia@naver.com>
2020-11-24 04:00:07 +0900

    5번 완료?

--- SmileGame/Assets/code/game6/EggSize.cs
+++ SmileGame/Assets/code/game6/EggSize.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class EggSize : MonoBehaviour
 {
+    Count c = new Count();
+    bool tf = false;
+    private void OnMouseDrag()
+    {
+        Debug.Log("왜잉래");
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        //마우스 좌표를 스크린 투 월드로 바꾸고 이 객체의 위치로 설정해 준다.
+        this.transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        if (transform.localScale.x > 0.5f && transform.localScale.y > 0.5f)
+        {
+            if (transform.localPosition.y > -0.2)
+            {
+                transform.localPosition = new Vector3(-0.012f, -1.802f, 1);
+            }
+        }
+            
+            
+        
+    }
 
     private float initTouchDist; // Touch Point 1 and 2's Distance
     Vector3 pos;
@@ -19,11 +39,17 @@ public class EggSize : MonoBehaviour
                 if (transform.localScale != new Vector3(0.5f, 0.5f, 1))
                 {
                     transform.localScale = new Vector3(transform.localScale.x - 0.05f, transform.localScale.y - 0.05f, transform.localScale.z);
-                    nsp = FindObjectOfType<NextStagePanel>();
-                    nsp.NextPanel();
                 }
             }
             initTouchDist = Vector2.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
+        }
+        if (transform.localScale == new Vector3(0.5f, 0.5f, 1))
+        {
+            if (transform.localPosition.y > 2)
+            {
+                nsp = FindObjectOfType<NextStagePanel>();
+                nsp.NextPanel();
+            }
         }
     }
 }
